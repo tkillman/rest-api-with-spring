f1f06c22c33b886614fc524921d81e35fcd27ffa
kimddonggyu <dkkim1004@hanmail.net>
2020-08-08 19:31:29 +0900

    test 환경설정
    postgresql 설치
    password pass

--- src/test/java/com/femis/events/EventControllerTests.java
+++ src/test/java/com/femis/events/EventControllerTests.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -30,6 +31,7 @@ import org.hamcrest.Matchers;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class EventControllerTests {
 	
 	@Autowired
